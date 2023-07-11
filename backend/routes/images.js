ef61fa49f3715df018f5cfe44108fc0e15a28b24
chof16 <javitenista46@gmail.com>
2023-07-11 14:12:19 +0200

    wiki casi terminada

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -2,18 +2,14 @@ var express = require('express');
 var router = express.Router();
 const axios = require("axios")
 
-const url = `https://api.thecatapi.com/v1/images/search?limit=9`;
+const url = "https://api.thecatapi.com/v1/images/search?limit=10";
 const api_key = "live_01T2OeMxgOePLxsmYF1GcByPq5dBXPmYCi7oezd5Vud75CbGxLAXRAvzqYxOd3II"
-let config = {
-  headers: {
-    'x-api-key': api_key
-  }
-}
 
 /* GET users listing. */
 router.get('/:id', async function (req, res, next) {
   const id = req.params.id
-  api = await axios.get(url + `&breeds_id=${id}`, config)
+  console.log(url + `&breeds_id=${id}`)
+  api = await axios.get(url + `&breed_ids=${id}&api_key=${api_key}`)
   json = api.data;
   console.log(json)
   res.send(json)
