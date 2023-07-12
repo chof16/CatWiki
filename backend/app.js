6fea8877686d4bd11f20e4808b65a42e6628ae60
chof16 <javitenista46@gmail.com>
2023-07-12 11:12:15 +0200

    Breeds view update

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/cats', catsRouter);
 app.use('/api/breeds', breedsRouter);
